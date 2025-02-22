ada09dde72d1ef31b3e669af8c72257cf4356bef
PineApples11 <kinyanjuiivy1@gmail.com>
2025-02-22 11:35:06 +0300

    inheritance practise

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,14 @@
 from user import User
 
 class Student(User):
+    # knowledge=[]
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        # self.first_name=first_name
+        # self.last_name=last_name
+        self.knowledge=[]
     
-    def learn(self):
-        pass
+    def learn(self,new_knowledge):
+        # if new_knowledge == str:
+            self.knowledge.append(new_knowledge)
+        
